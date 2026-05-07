30907ef25cc9d8c876ef6a4e2fcd0d92250cea08
Govind <govindmane@bitlasoft.com>
2026-05-07 12:21:39 +0530

    update web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3443130565"
+    serviceWorkerVersion: "1927596112"
   }
 });
